c0f0af9762ee07bfadff69728293ed7f5a8008ff
Andrew0502 <agcmarcus@gmail.com>
2020-11-03 20:18:05 -0500

    Made changes to deploy to heroku.

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+if(process.env.NODE_ENV === "production") {
+app.use(express.static("client/build"));
+}
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/react-portfolio", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -32,6 +36,10 @@ app.get("/api/config", (req, res) => {
     success: true,
   });
 });
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`);
