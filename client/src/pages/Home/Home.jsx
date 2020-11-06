269b404e3a561e6b6fa827592df2fe9c0c21f23c
Andrew0502 <agcmarcus@gmail.com>
2020-11-05 20:57:34 -0500

    Added styling to Home.jsx .

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -28,8 +28,8 @@ const Home = () => {
 
               <p>
                 Hello! I am a technology student who is attending the Georgia Tech Coding Bootcamp to broaden my horizons.
-                I live in Ringgold, Georgia with my parents. I am an analytical thinker with a eye for detail. I thrive
-                on feedback and am a joy to work with. Don't just ask me ask my references. Before Coronavirus, I worked as an 
+                I live in Ringgold, Georgia. I am an analytical thinker with a eye for detail. I thrive
+                on feedback and am a joy to work with. Don't just ask me, ask my references. Before Coronavirus, I worked as an 
                 intern at Unum in Chattanooga, TN. I was also finishing my degree in management of information systems. In 2016 I graduated
                 with a degree in international studies from Berry College, in Rome, Georgia. My work experiences include 7 months at Walt
                 Disney World Resort in Orlando, Florida. I chose to take this bootcamp to boost my skills and start a career.
@@ -40,14 +40,13 @@ const Home = () => {
           <div className="row">
             <p>
               My skills include React, Javascript, Node.js, CSS, MySQL, MongoDB, Express.js, Customer Service, and some experience with International Business.
-              I have a degree in International Studies from Berry College and a degree in Management of Information Systems from Dalton State College. 
-              My most significant work experiences are working for Unum insurance company and for Walt Disney World resorts. Both as and Intern. You can connect with Me on{" "}
+              You can connect with Me on{" "}
               <a href="www.linkedin.com/in/andrew-marcus1">
                 LinkedIn
               </a>{" "}
               and view his website portfolio at{" "}
               <a href="https://github.com/7J647"> Github</a>. Also, here is a
-              link to Jeff's{" "}
+              link to my{" "}
               <a href={pdf} target="_blank" rel="noreferrer" >Resume.
               </a>{" "}
               Thank you for visiting!
